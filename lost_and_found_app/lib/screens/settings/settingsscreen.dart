9f7d63c4526fe946afa5561c3dabf55a21150b8e
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-06-18 22:20:53 +0300

    added logout to settings page

--- lost_and_found_app/lib/screens/settings/settingsscreen.dart
+++ lost_and_found_app/lib/screens/settings/settingsscreen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../authentication/auth.dart';
+import '../../widgets/long_button.dart';
+import '../starting screens/getStarted_screen.dart';
+
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
@@ -8,11 +12,28 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  final Auth _auth = Auth();
+
+  void _handleLogout(BuildContext context) async {
+    await _auth.logout();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GetStartedScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Settings Screen"),
+        child: CustomLongButton(
+          text: "Logout",
+          onPressed: () {
+            _handleLogout(context);
+          },
+        ),
       ),
     );
   }
